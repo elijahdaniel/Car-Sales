823f7b2fbd93b06a929c56f68550b253f07fe1ec
Elijah Paminsan <e.paminsan@gmail.com>
2019-09-29 08:33:20 -0700

    mvp

--- src/App.js
+++ src/App.js
@@ -1,48 +1,44 @@
-import React from 'react';
+import React from 'react'
 
-import Header from './components/Header';
-import AddedFeatures from './components/AddedFeatures';
-import AdditionalFeatures from './components/AdditionalFeatures';
-import Total from './components/Total';
+import { useSelector, useDispatch } from 'react-redux'
+import { ADD_ITEM, REMOVE } from './actions'
+
+import Header from './components/Header'
+import AddedFeatures from './components/AddedFeatures'
+import AdditionalFeatures from './components/AdditionalFeatures'
+import Total from './components/Total'
 
 const App = () => {
-  const state = {
-    additionalPrice: 0,
-    car: {
-      price: 26395,
-      name: '2019 Ford Mustang',
-      image:
-        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-      features: []
-    },
-    store: [
-      { id: 1, name: 'V-6 engine', price: 1500 },
-      { id: 2, name: 'Racing detail package', price: 1500 },
-      { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 }
-    ]
-  };
+  const dispatch = useDispatch()
+
+  const additionalPrice = useSelector(state => state.additionalPrice)
+  const car = useSelector(state => state.car)
+  const store = useSelector(state => state.store)
 
   const removeFeature = item => {
     // dispatch an action here to remove an item
-  };
+    console.log(item)
+    dispatch({ type: REMOVE, payload: item })
+  }
 
   const buyItem = item => {
     // dipsatch an action here to add an item
-  };
+    let index = car.features.findIndex(element => element.name === item.name)
+    index === -1 && dispatch({ type: ADD_ITEM, payload: item })
+  }
 
   return (
-    <div className="boxes">
-      <div className="box">
-        <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+    <div className='boxes'>
+      <div className='box'>
+        <Header car={car} />
+        <AddedFeatures car={car} removeFeature={removeFeature} />
       </div>
-      <div className="box">
-        <AdditionalFeatures store={state.store} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+      <div className='box'>
+        <AdditionalFeatures store={store} buyItem={buyItem} />
+        <Total car={car} additionalPrice={additionalPrice} />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from 'react'
 
 const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button onClick={() => props.remove(props.feature)} className='button'>
+        X
+      </button>
       {props.feature.name}
     </li>
-  );
-};
+  )
+}
 
-export default AddedFeature;
+export default AddedFeature
